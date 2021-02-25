7ba9b5f138b5dc49aec65f9b88ef4463196a4f7f
Oliver Coldwell <olivercoldwell@gmail.com>
2021-02-25 18:27:59 +0000

    basic web functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,11 @@ import { translator } from './translator.js'
 
 document.getElementById('hidden-enter').addEventListener('click', ()=>{
   let toBeTranslated= document.getElementById('translation-input').value;
-  translator(toBeTranslated);
+  document.getElementById("translated-output").innerHTML = translator(toBeTranslated);
+})
+
+document.getElementById('translation-input').addEventListener('keyup', () =>{
+  let toBeTranslated = document.getElementById('translation-input').value;
+  document.getElementById("translated-output").innerHTML = translator(toBeTranslated);
 })
 
